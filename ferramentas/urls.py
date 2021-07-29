c0195cdfe8cb1558dbe20d43d6d2992494e8c6bf
Higor Gustavo <higor-gustavo_lp@hotmail.com>
2021-07-29 20:20:35 -0300

    update manutenção

--- ferramentas/urls.py
+++ ferramentas/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("ferramenta/<int:id>/update", update_ferramenta, name='update_ferramenta'),
     path("ferramenta/<int:id>/manutencao/add", create_manutencao, name='create_manutencao'),
     path("manutencao/<int:id>/visualizar", detail_manutencao, name='detail_manutencao'),
+    path("manutencao/<int:id>/update", update_manutencao, name='update_manutencao'),
 ]
--- ferramentas/views/manutencao_views.py
+++ ferramentas/views/manutencao_views.py
@@ -42,3 +42,49 @@ def create_manutencao(request, id):
             }
             return render(request, "manutencoes/create.html", context)
 
+
+def update_manutencao(request, id):
+    manutencao = get_object_or_404(Manutencao, id=id)
+    feramenta = manutencao.ferramenta
+    form = ManutencaoForm(instance=manutencao)
+    if request.method == "POST":
+        form = ManutencaoForm(request.POST, instance=manutencao)
+        if form.is_valid():
+            manutencao = form.save(commit=False)
+            manutencao.ferramenta = feramenta
+            manutencao.save()
+            if manutencao.status_manutencao == "Atrasada":
+                messages.add_message(request, messages.WARNING, 'Manutenção ATRASADA!!!')
+            else:
+                messages.add_message(request, messages.SUCCESS,
+                                     'Manutenção ' + manutencao.status_manutencao + ' com Sucesso!')
+            return redirect('/ferramenta/' + str(feramenta.id))
+
+    context = {
+        'ferramenta': feramenta,
+        'form': form
+    }
+    return render(request, "manutencoes/update.html", context)
+
+# def agendamento_rapido(request, id):
+#     ferramenta = get_object_or_404(Ferramenta, id=id)
+#
+#     if request.method == "GET":
+#         form = ManutencaoForm()
+#         context = {
+#             'ferramenta': ferramenta,
+#             'form': form,
+#         }
+#         print("Aqui")
+#         return render(request, "manutencoes/agendamento_rapido.html", context)
+#
+#     elif request.method == "POST":
+#         print("POST")
+#         form = ManutencaoForm(request.POST)
+#         if form.is_valid():
+#             manutencao = form.save(commit=False)
+#             manutencao.ferramenta = ferramenta
+#             manutencao.status_manutencao = "Agendada"
+#             manutencao.save()
+#             messages.add_message(request, messages.SUCCESS, "Manutenção agendada com sucesso")
+#             return redirect('list_clintes')
