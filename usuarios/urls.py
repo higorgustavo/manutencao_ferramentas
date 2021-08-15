d8d171641d12d3fcb04fa15b99b2d571f863825b
Higor Gustavo <higor-gustavo_lp@hotmail.com>
2021-08-15 11:51:18 -0300

    add dashboard

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -3,4 +3,5 @@ from usuarios.views.pages_views import *
 
 urlpatterns = [
     path("", dashboard, name='dashboad'),
+    path("refresh", refresh_datas, name='refresh_datas'),
 ]
--- usuarios/views/pages_views.py
+++ usuarios/views/pages_views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Q
 from ferramentas.models import Manutencao
+from django.contrib import messages
 from datetime import date
+
+
+def refresh_datas(request):
+    manutencoes = Manutencao.objects.all()
+    for manutencao in manutencoes:
+        if manutencao.status_manutencao == "Agendada" and manutencao.data_manutencao < date.today():
+            manutencao.status_manutencao = "Atrasada"
+            Manutencao.objects.bulk_update(manutencoes, ['status_manutencao'])
+        if manutencao.status_manutencao == "Atrasada" and manutencao.data_manutencao >= date.today():
+            manutencao.status_manutencao = "Agendada"
+            Manutencao.objects.bulk_update(manutencoes, ['status_manutencao'])
+    messages.add_message(request, messages.SUCCESS, "Agendamentos atualizados com Sucesso!")
+    return redirect('dashboad')
 
 
 def dashboard(request):
